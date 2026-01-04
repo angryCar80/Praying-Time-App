121beaa34cb1c27a2c4c1f8bfae0d90028ac887f
angryCar80 <ouabbasaymen@gmail.com>
2026-01-04 18:34:13 +0100

    feat: the times are showing

--- index.ts
+++ index.ts
@@ -1,21 +1,45 @@
 import chalk from "chalk";
 
-const args: string[] = process.argv.slice(2);
-const flags: string[] = ["--help", "--country"];
+// let country_op: string;
+// let city_op: string;
 
-function readApi() {
+let API_URL: string = "https://api.aladhan.com/v1/timingsByCity?city=Algiers&country=Algeria";
+
+
+const args: string[] = process.argv.slice(2);
+const flags: string[] = ["help", "country"];
+
+async function readApi(args: string[]) {
+  try {
+
+    const query = args[0] ?? "default";
+
+    const res = await fetch(`${API_URL}?q=${query}`);
+
+    if (!res.ok) {
+      throw new Error(`API error: ${res.status}`);
+    }
+
+    const data = await res.json();
+
+    console.log("Result: ");
+    console.log(data);
+
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
 }
 
 function main() {
   if (args.length < 0) {
     chalk.white.bgRed.bold("WRONG ARGUMNETS");
   } if (args[0] == "--help") {
-
-    let i: number = 0;
+    let flagsDisplay: string = flags.join("\n");
     console.log("available arguments:");
-    for (i < flags.length; i++;) {
-      console.log(flags[i]);
-    }
+    console.log(flagsDisplay);
+  }
+  else if (args[0] == "--country") {
+    readApi(args);
   }
   else {
     console.log(chalk.white.bgRed.bold("WRONG ARGUMNETS"));
